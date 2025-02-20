93b27fbd0bc6db93e981b353d02f971299466807
Jorge Sanchez <jsanchez.contact29@gmail.com>
2025-02-20 10:35:24 +0100

    Ejercicios operadores

---
+++ 4. Operadores/ejercicio_01.py
@@ -0,0 +1,32 @@
+# Operadores aritmeticos
+
+a =10
+b = 3
+
+# Suma
+suma = a +b
+print(f'Suma: {suma}')
+
+# Resta
+resta = a-b
+print(f'Resta: {resta}')
+
+# Multiplicacion
+multiplicacion = a * b
+print(f'Multiplicacion: {multiplicacion}')
+
+# Division
+division = a/b
+print(f'Division: {division:.2f}')
+
+# Division entera
+division_entera = a//b
+print(f'Division entera: {division_entera}')
+
+# Modulo
+modulo = a%b
+print(f'Modulo de division = {modulo}')
+
+# Potencia
+exponente = a**b
+print(f'Exponente: {exponente}')
---
+++ 4. Operadores/ejercicio_02.py
@@ -0,0 +1,15 @@
+numero = 5
+print(f'Valor del numero: {numero}')
+numero = 7
+print(f'Valor del numero: {numero}')
+cadena = 'Hola ;)'
+numero = 5
+print(f'Valor de la cadena: {cadena}')
+
+# Asignacion multiple
+x,y,z = 5,"Hola",4.2863
+print(f'Valor de x = {x},y = {y}, z = {z:.2f}')
+
+# Asignacion encadenada
+a = b = c = 10
+print(f'Valor a = {a}, b = {b}, c = {c}')
